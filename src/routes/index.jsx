925fbc99562086b6fe18ee085b68ab38fdffbeaf
Laiba-Asif <laibae.awan@gmail.com>
2025-06-12 03:58:47 +0500

    services pages remived

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '../Pages/Home';
-import Projects from '../Pages/Projects';
-import Services from '../Pages/Services';
+// import Projects from '../Pages/Projects';
+// import Services from '../Pages/Services';
 
 const MyRoutes = () => {
   return (
    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/projects" element={<Projects />} />
+        {/* <Route path="/services" element={<Services />} />
+        <Route path="/projects" element={<Projects />} /> */}
         {/* <Route path="/articles" element={<Home />} /> */}
       </Routes>
     </Router>
